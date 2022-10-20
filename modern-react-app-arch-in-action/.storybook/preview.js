4f65a5b695ecb12c0ce177a730ff811365114d86
abdullah <44318011+fx2y@users.noreply.github.com>
2022-10-21 03:55:43 +0700

    update: modern-react-app-arch-in-action
    Signed-off-by: abdullah <44318011+fx2y@users.noreply.github.com>

---
+++ modern-react-app-arch-in-action/.storybook/preview.js
@@ -0,0 +1,16 @@
+import {themes} from '../src/config/theme';
+
+export const parameters = {
+    actions: {argTypesRegex: '^on[A-Z].*'},
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+        expanded: true,
+
+    },
+    chakra: {
+        theme: themes,
+    },
+};
